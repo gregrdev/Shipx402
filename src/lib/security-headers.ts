c8fb845859faa2cf3cd0145f3212948770fe7db3
Phoenix <grachkovsky@gmail.com>
2026-08-04 06:34:16 -0400

    Fix CSP: allow inline scripts for hydration bootstrap

--- src/lib/security-headers.ts
+++ src/lib/security-headers.ts
@@ -10,7 +10,7 @@ export const CONTENT_SECURITY_POLICY = [
   "object-src 'none'",
   "frame-ancestors 'none'",
   "form-action 'self'",
-  "script-src 'self'",
+  "script-src 'self' 'unsafe-inline'",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' https://fonts.gstatic.com data:",
   "img-src 'self' data: blob:",
